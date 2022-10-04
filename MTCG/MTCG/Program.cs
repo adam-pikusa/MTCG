81877c7500383608fd2e958554e6f02c9fdc3a86
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2022-10-04 15:37:12 +0200

    added more battle examples

--- MTCG/MTCG/Program.cs
+++ MTCG/MTCG/Program.cs
@@ -13,12 +13,21 @@ var allCards = CardDeserializer.deserializeAllCards(File.ReadAllText(assetFile))
 //The FireElves know Dragons since they were little and can evade their attacks. 
 
 var goblin = allCards[0];
+var knight = allCards[3];
 var dragon = allCards[5];
 var fireElf = allCards[6];
+var fireSpell = allCards[7];
+var waterSpell = allCards[8];
 
-var deckA = new Deck() { goblin };
-var deckB = new Deck() { dragon };
-var deckC = new Deck() { fireElf };
+
+var deckA = new Deck { goblin };
+var deckB = new Deck { dragon };
+var deckC = new Deck { fireElf };
+var deckD = new Deck { knight };
+var deckE = new Deck { fireSpell };
+var deckF = new Deck { waterSpell };
 
 Battle.Fight(deckA, deckB);
 Battle.Fight(deckB, deckC);
+Battle.Fight(deckD, deckE);
+Battle.Fight(deckD, deckF);
