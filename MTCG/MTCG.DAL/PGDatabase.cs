8b4d433dbe42303e29d15d87e5a26d5ec7aef181
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2022-11-30 10:13:52 +0100

    added database interface

---
+++ MTCG/MTCG.DAL/PGDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTCG.DAL
+{
+    public class PGDatabase : IDatabase
+    {
+        static PGDatabase instance;
+        static PGDatabase() => instance = new PGDatabase();
+
+        private PGDatabase() 
+        {
+            Console.WriteLine("Database init");
+        }
+
+        public IDatabase Instance() => instance;
+
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello world from DB");
+        }
+    }
+}
